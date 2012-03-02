9060b25e0cacf4402d5b28a1261cac3f29749722
Rob Dotson <rld215@nyu.edu>
2012-03-02 13:59:44 -0800

    Changed company name and copyright info

--- Framework/CoreAcquisition/CoreAcquisition/CoreAcquisition.h
+++ Framework/CoreAcquisition/CoreAcquisition/CoreAcquisition.h
@@ -3,7 +3,7 @@
 //  CoreAcquisition
 //
 //  Created by Rob Dotson on 3/2/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Rob Dotson, Center for Neural Science, New York University. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
